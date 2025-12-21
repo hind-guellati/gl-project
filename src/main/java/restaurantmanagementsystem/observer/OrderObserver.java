8198c5fac97f215bcda1de45dd8d6707effd1c42
Nazim <nazimguerguadj@gmail.com>
2025-12-21 23:56:07 +0100

    Update OrderObserver.java

--- src/main/java/restaurantmanagementsystem/observer/OrderObserver.java
+++ src/main/java/restaurantmanagementsystem/observer/OrderObserver.java
@@ -1,7 +1,8 @@
 package restaurantmanagementsystem.observer;
 
-public interface Observable {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers(String message);
+public class OrderObserver implements Observer {
+    @Override
+    public void update(String message){
+        System.out.println(message);
+    }
 }
